bceca2578cdb1faf41518b73c98d87a50e71f3a3
Eng. Lalai Samuel Ntongai <159328526+Engineer-Samuel-Lalai@users.noreply.github.com>
2024-08-18 16:19:04 +0300

    Create Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,9 @@
+#Whats your name
+name = input("What's your name ") 
+
+#("Whats your Age: ")
+age = input("What is your age ")
+
+#What is your location
+location = input("What's your location ")
+print("Hello ", name,"you are ", age," years old and live in ", location)
